fd6d46ac80e7a10e2eac724b77308b8f600406e7
ramirosomavilla <rami@skaterelephant.com>
2025-05-28 09:29:22 -0300

    add user service && minnor changes

---
+++ servicio-usuarios/src/main/java/serviciousuarios/ServicioUsuariosApplication.java
@@ -0,0 +1,11 @@
+package serviciousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuariosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioUsuariosApplication.class, args);
+    }
+}
