9f409229b7610d25ff1c85cfcf51896f50b3575d
ramirosomavilla <rami@skaterelephant.com>
2025-05-27 22:20:47 -0300

    add basic notification service endpoint

---
+++ servicio-notificaciones/src/main/java/servicionotificaciones/controllers/NotificacionController.java
@@ -0,0 +1,23 @@
+package servicionotificaciones.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notificaciones")
+public class NotificacionController {
+    private static final Logger logger = LoggerFactory.getLogger(NotificacionController.class);
+
+    @PostMapping
+    public String recibirNotificacion(@RequestBody(required = false) String body) {
+        logger.info("¡Petición recibida en /notificaciones! Cuerpo: {}", body);
+        return "Notificación recibida";
+    }
+
+    @GetMapping
+    public String test() {
+        logger.info("¡Petición GET recibida en /notificaciones!");
+        return "Servicio de notificaciones activo";
+    }
+}
