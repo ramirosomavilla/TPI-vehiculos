eaaa45b92d56b6a6b542ef037b414e0e04edbc93
ramirosomavilla <rami@skaterelephant.com>
2025-06-27 16:12:42 -0300

    delete personal info

--- servicio-usuarios/src/main/java/serviciousuarios/config/SwaggerConfig.java
+++ servicio-usuarios/src/main/java/serviciousuarios/config/SwaggerConfig.java
@@ -13,27 +13,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-  @Bean
-  public OpenAPI customOpenAPI() {
-    final String securitySchemeName = "x-internal-key";
-    return new OpenAPI()
-        .info(new Info()
-            .title("API REST Usuarios")
-            .version("0.0")
-            .description("Spring Boot && OpenAPI API REST Usuarios")
-            .contact(new Contact()
-                .name("Ramiro Somavilla")
-                .email("somavillarami@gmail.com"))
-            .license(new License()
-                .name("Apache 2.0")
-                .url("https://www.apache.org/licenses/LICENSE-2.0")))
-        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-        .components(new Components()
-            .addSecuritySchemes(securitySchemeName,
-                new SecurityScheme()
-                    .name("X-Internal-Token")
-                    .type(SecurityScheme.Type.APIKEY)
-                    .in(SecurityScheme.In.HEADER)
-                    .name("X-Internal-Token")));
-  }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "x-internal-key";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API REST Usuarios")
+                        .version("0.0")
+                        .description("Spring Boot && OpenAPI API REST Usuarios")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name("X-Internal-Token")
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-Internal-Token")));
+    }
 }
